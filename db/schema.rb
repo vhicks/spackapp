f41bf884663c0b334d6b1d9f02221e09a38d7992
Roni Hicks <veronicahicks@uchicago.edu>
2016-12-21 23:16:02 -0600

    Add postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161217165130) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "visitors", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
